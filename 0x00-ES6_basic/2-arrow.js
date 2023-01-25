602f2bbc322e46388e647f2b22b9b648090bdcb9
Ella-1 <es9897288@gmail.com>
2023-01-25 03:24:46 -0800

    task 3

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
-
 /* eslint-disable */
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
