a04b10210c97b43ca9794fe1fa17ff081a5edf00
Ella-1 <es9897288@gmail.com>
2023-01-28 01:56:44 -0800

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,30 @@
-import Currency from "./3-currency";
-
 export default class Pricing {
-    constructor(amount, currency){
-        this_amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(n){
-        return this._amount=n;
-    }
+  set amount(n) {
+    this._amount = n;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(n) {
-        return this._currency = n;
-    }
+  set currency(n) {
+    this._currency = n;
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${Currency._name} (${Currency.code})`
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    static conversionRate(number,conversionRate) {
-        return number * conversionRate
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
