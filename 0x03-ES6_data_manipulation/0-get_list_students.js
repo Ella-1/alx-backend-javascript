2fa06a86950238dff281bb5b0a5d848eb8f7a28c
Ella-1 <es9897288@gmail.com>
2023-01-30 10:00:50 -0800

    task 1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,7 +3,7 @@ const getListStudents = () => {
   const student_1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student_2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const student_3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-  students.push(student1, student2, student3);
+  students.push(student_1, student_2, student_3);
 
   return students;
 };
